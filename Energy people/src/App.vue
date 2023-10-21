7bf4b8cbd74d24152bb7279d0641f9bf5ccaaa6c
igait <garikrulezzz@mail.ru>
2023-10-21 20:23:57 +0300

    new file:   Energy people/.gitignore 	new file:   Energy people/.vscode/extensions.json 	new file:   Energy people/README.md 	new file:   Energy people/index.html 	new file:   Energy people/package-lock.json 	new file:   Energy people/package.json 	new file:   Energy people/public/favicon.ico 	new file:   Energy people/src/App.vue 	new file:   Energy people/src/main.js 	new file:   Energy people/vite.config.js

---
+++ Energy people/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <!-- Дана кнопка. Дан массив. Выведите элементы этого массива в виде списка ul. Сделайте так, 
+    чтобы по нажатию на кнопку в конец этого списка добавился новый пункт. -->
+    <p :class="obj">home</p>
+    <button @click="show">show</button>
+
+</template>
+
+<script>
+	export default{
+        data() {
+            return {
+                obj: {
+                    hidden: true,
+                },
+            }
+        },
+        methods: {
+            show(){
+                this.obj.hidden = !this.obj.hidden;
+            }
+        }
+    }
+</script>
+<style>
+
+    /* table{
+        border-collapse: collapse;
+        width: 30%;
+        text-align: center;
+    }
+    td{
+        padding: 5px;
+        border: 1px solid #000;
+    } */
+    p.hidden {
+	    display: none;
+    }
+
+    li{
+        color: red;
+    }
+
+</style>
